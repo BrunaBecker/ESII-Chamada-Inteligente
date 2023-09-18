acc3b93a2582f2ef8defd877050a3f4ba493d07a
Maximilian Harrisson <maximilianharrisson@gmail.com>
2023-09-18 00:21:37 -0300

    Add authentication loading

--- lib/app/pages/login/login_controller.dart
+++ lib/app/pages/login/login_controller.dart
@@ -1,11 +1,25 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginController extends GetxController {
+  final _isLoading = false.obs;
+
   final _isVisible = false.obs;
+
+  bool get isLoading => _isLoading.value;
 
   bool get isVisible => _isVisible.value;
 
   void togglePasswordVisibility() {
     _isVisible.value = !_isVisible.value;
+  }
+
+  Future<void> authenticate() async {
+    // TODO: validate form and authentication
+    _isLoading.value = true;
+    debugPrint("Authenticating");
+    await Future.delayed(const Duration(seconds: 3));
+    debugPrint("Success");
+    _isLoading.value = false;
   }
 }
