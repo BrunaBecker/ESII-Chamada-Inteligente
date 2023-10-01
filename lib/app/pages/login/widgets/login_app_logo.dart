cafab79df608740101de85a4fba185dfa8f24457
Maximilian Harrisson <maximilianharrisson@gmail.com>
2023-10-01 00:23:05 -0300

    Add app logo asset

--- lib/app/pages/login/widgets/login_app_logo.dart
+++ lib/app/pages/login/widgets/login_app_logo.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
-import '../../../core/theme/app_colors.dart';
+import '../../../core/theme/app_assets.dart';
 
 class LoginAppLogo extends StatelessWidget {
   const LoginAppLogo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: 200,
-      height: 160,
-      color: AppColors.lightGray,
+      height: 200,
+      child: Image.asset(
+        ImageAssets.appLogo,
+        fit: BoxFit.contain,
+      ),
     );
   }
 }
