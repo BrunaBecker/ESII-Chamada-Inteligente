3b68a99fa7a4bc97da80567185a1bf209d03e4cd
motadv <102678270+motadv@users.noreply.github.com>
2023-10-19 18:56:39 -0300

    WIP comms com API

--- lib/app/core/adapters/location_adapter.dart
+++ lib/app/core/adapters/location_adapter.dart
@@ -1,43 +1,42 @@
 import 'package:location/location.dart';
 
 class LocationAdapter {
+  Location location = Location();
+  bool _serviceEnabled = false;
+  PermissionStatus? _permissionGranted;
+  LocationData? _locationData;
 
-    Location location = Location();
-    bool _serviceEnabled = false;
-    PermissionStatus _permissionGranted;
-    LocationData _locationData;
+  Future<bool> isLocationServiceEnabled() async {
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-    bool isLocationServiceEnabled() async {
-        _serviceEnabled = await location.serviceEnabled();
-        if (!_serviceEnabled) {
-            _serviceEnabled = await location.requestService();
-            if (!_serviceEnabled) {
-                return false;
-            }
-        }
-        return true;
+  Future<bool> checkLocationPermission() async {
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  Future<LocationData?> getCurrentLocation() async {
+    bool hasPermission = await checkLocationPermission();
+    bool locationServiceEnabled = await isLocationServiceEnabled();
+
+    if (!hasPermission || !locationServiceEnabled) {
+      return null;
     }
 
-    bool checkLocationPermission() async {
-        _permissionGranted = await location.hasPermission();
-        if (_permissionGranted == PermissionStatus.denied) {
-            _permissionGranted = await location.requestPermission();
-            if (_permissionGranted != PermissionStatus.granted) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    Future<LocationData> getCurrentLocation() async {
-        hasPermission = await checkLocationPermission();
-        isLocationServiceEnabled = await isLocationServiceEnabled();
-
-        if(!hasPermission || !isLocationServiceEnabled) {
-            return null;
-        }
-
-        _locationData = await location.getLocation();
-        return _locationData;
-    }
+    _locationData = await location.getLocation();
+    return _locationData;
+  }
 }
