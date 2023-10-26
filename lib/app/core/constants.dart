f5239a86c71a16c74734f7b68b27016818e9f6db
Maximilian Harrisson <maximilianharrisson@gmail.com>
2023-10-26 02:03:54 -0300

    Update date and time patterns

--- lib/app/core/constants.dart
+++ lib/app/core/constants.dart
@@ -1,2 +1,5 @@
-const appDatePattern = "dd/MM/yyyy hh:mm:ss";
-const storageDatePattern = "yyyy-MM-dd hh:mm:ss";
+const appDateTimePattern = "dd/MM/yyyy hh:mm:ss";
+const storageDateTimePattern = "yyyy-MM-dd hh:mm:ss";
+
+const appDatePattern = "dd/MM/yyyy";
+const storageDatePattern = "yyyy-MM-dd";
