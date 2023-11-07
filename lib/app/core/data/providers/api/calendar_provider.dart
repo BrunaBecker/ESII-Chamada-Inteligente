46c19902c3ed17a32f00152c7b98ab3b0071cf7b
motadv <102678270+motadv@users.noreply.github.com>
2023-11-07 20:38:43 -0300

    🚨Unused Import

--- lib/app/core/data/providers/api/calendar_provider.dart
+++ lib/app/core/data/providers/api/calendar_provider.dart
@@ -1,5 +1,4 @@
 import '../../../adapters/http_adapter.dart';
-import '../../../domain/entities/calendar_entity.dart';
 import '../base_provider.dart';
 
 class CalendarProvider extends BaseProvider {
