ad066fe3b5efc3e438558c2fcfd13716087dfde5
Maximilian Harrisson <maximilianharrisson@gmail.com>
2023-11-07 21:38:43 -0300

    :ambulance: Fix geolocator build error

--- lib/app/core/adapters/macfi_adapter.dart
+++ lib/app/core/adapters/macfi_adapter.dart
@@ -79,8 +79,7 @@ class MacFiAdapter {
 
   Future<Placemark> getPlacemark() async {
     position = await geolocatorAdapter.determinePosition();
-    List<Placemark> placemarks =
-        await placemarkFromCoordinates(position!.latitude, position!.longitude);
+    List<Placemark> placemarks = await placemarkFromCoordinates(position!.latitude, position!.longitude);
     return placemarks.first;
   }
 }
