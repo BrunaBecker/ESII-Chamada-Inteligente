3c24bd3dd0a30e0d5bb42147cbd8a8d972110e0c
BrunaBecker <brunabecker@id.uff.br>
2023-11-11 08:39:57 -0300

    Update run_integration_tests.sh

--- test/integration_test/scripts/run_integration_tests.sh
+++ test/integration_test/scripts/run_integration_tests.sh
@@ -2,11 +2,11 @@
 
 # Define an array of test files
 TEST_FILES=(
-  "/home/runner/work/ESII-Chamada-Inteligente/ESII-Chamada-Inteligente/test/integration_test/login_test.dart"
-  "/home/runner/work/ESII-Chamada-Inteligente/ESII-Chamada-Inteligente/test/integration_test/notification_test.dart"
+  "test/integration_test/login_test.dart"
+  "test/integration_test/notification_test.dart"
 )
 
 # Iterate over the array and run each test
 for testFile in "${TEST_FILES[@]}"; do
-  flutter drive --target="$testFile"
+  flutter test "$testFile"
 done
