64789b5afd8c5aa85dae55b1f37767c7335bcb40
Asunnya <asunnyya@gmail.com>
2023-11-20 22:01:09 -0300

    :alien: updated calendar provider

--- lib/app/core/data/providers/api/calendar_provider.dart
+++ lib/app/core/data/providers/api/calendar_provider.dart
@@ -2,6 +2,7 @@ import '../../../adapters/http_adapter.dart';
 import '../../../domain/entities/calendar_entity.dart';
 import '../../../exceptions/entity_not_found_exception.dart';
 import '../../../exceptions/no_api_response_exception.dart';
+import '../../../exceptions/unexpected_api_exception.dart';
 import '../../dtos/calendar_dto.dart';
 import '../base_provider.dart';
 
@@ -10,38 +11,15 @@ class CalendarProvider extends BaseProvider {
 
   CalendarProvider({required this.http});
 
-  Future<CalendarEntity?> fetchById(int calendarId) async {
+  Future<CalendarEntity> fetchById() async {
     try {
       final response = await http.get(
-        '/calendar/$calendarId',
-      );
-
-      validateResponse(
-        response: response,
-      );
-
-      return CalendarDto.fromMap(response.data);
-    } on EntityNotFoundException {
-      rethrow;
-    } on NoApiResponseException {
-      rethrow;
-    } catch (e) {
-      logError(e.toString());
-      return null;
-    }
-  }
-
-  Future<CalendarEntity?> create(CalendarEntity calendarEntity) async {
-    CalendarDto calendarDto = CalendarDto.fromEntity(calendarEntity);
-
-    try {
-      final response = await http.post(
         '/calendar',
-        body: calendarDto.toJson(),
       );
 
       validateResponse(
         response: response,
+        statusCodes: [200],
       );
 
       return CalendarDto.fromMap(response.data);
@@ -51,21 +29,22 @@ class CalendarProvider extends BaseProvider {
       rethrow;
     } catch (e) {
       logError(e.toString());
-      return null;
+      throw UnexpectedApiException();
     }
   }
 
-  Future<CalendarEntity?> update(CalendarEntity calendarEntity) async {
+  Future<CalendarEntity> update(CalendarEntity calendarEntity) async {
     CalendarDto calendarDto = CalendarDto.fromEntity(calendarEntity);
 
     try {
       final response = await http.put(
         '/calendar',
-        body: calendarDto.toJson(),
+        body: calendarDto.toMap(),
       );
 
       validateResponse(
         response: response,
+        statusCodes: [200],
       );
 
       return CalendarDto.fromMap(response.data);
@@ -75,11 +54,11 @@ class CalendarProvider extends BaseProvider {
       rethrow;
     } catch (e) {
       logError(e.toString());
-      return null;
+      throw UnexpectedApiException();
     }
   }
 
-  Future<CalendarEntity?> setEvent(int calendarId, int eventId) async {
+  Future<CalendarEntity> setEvent(int calendarId, int eventId) async {
     try {
       final response = await http.put(
         '/calendar/setEvent',
@@ -91,6 +70,7 @@ class CalendarProvider extends BaseProvider {
 
       validateResponse(
         response: response,
+        statusCodes: [200],
       );
 
       return CalendarDto.fromMap(response.data);
@@ -100,12 +80,11 @@ class CalendarProvider extends BaseProvider {
       rethrow;
     } catch (e) {
       logError(e.toString());
-
-      return null;
+      throw UnexpectedApiException();
     }
   }
 
-  Future<CalendarEntity?> removeEvent(int calendarId, int eventId) async {
+  Future<CalendarEntity> removeEvent(int calendarId, int eventId) async {
     try {
       final response = await http.put(
         '/calendar/removeEvent',
@@ -117,6 +96,7 @@ class CalendarProvider extends BaseProvider {
 
       validateResponse(
         response: response,
+        statusCodes: [200],
       );
 
       return CalendarDto.fromMap(response.data);
@@ -126,8 +106,7 @@ class CalendarProvider extends BaseProvider {
       rethrow;
     } catch (e) {
       logError(e.toString());
-
-      return null;
+      throw UnexpectedApiException();
     }
   }
 }
