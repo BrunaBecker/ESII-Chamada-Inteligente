5a0ce8df40b4ada1ce03e7f686b4ccd3e88e9fcb
Asunnya <asunnyya@gmail.com>
2023-11-21 00:39:08 -0300

    :alien: updated virtual zone provider

--- lib/app/core/data/providers/api/virtual_zone_provider.dart
+++ lib/app/core/data/providers/api/virtual_zone_provider.dart
@@ -2,6 +2,7 @@ import '../../../adapters/http_adapter.dart';
 import '../../../domain/entities/virtual_zone_entity.dart';
 import '../../../exceptions/entity_not_found_exception.dart';
 import '../../../exceptions/no_api_response_exception.dart';
+import '../../../exceptions/unexpected_api_exception.dart';
 import '../../dtos/virtual_zone_dto.dart';
 import '../base_provider.dart';
 
@@ -10,18 +11,46 @@ class VirtualZoneProvider extends BaseProvider {
 
   VirtualZoneProvider({required this.http});
 
-  Future<VirtualZoneEntity?> create(VirtualZoneEntity virtualZoneEntity) async {
+  Future<List<VirtualZoneEntity>> fetchAll() async {
+    try {
+      final response = await http.get(
+        '/virtualZone',
+      );
+
+      validateResponse(
+        response: response,
+        statusCodes: [200],
+      );
+
+      final virtualZones = response.data
+          .map<VirtualZoneDto>(
+            (virtualZone) => VirtualZoneDto.fromMap(virtualZone),
+          )
+          .toList();
+
+      return virtualZones;
+    } on EntityNotFoundException {
+      rethrow;
+    } on NoApiResponseException {
+      rethrow;
+    } catch (e) {
+      logError(e.toString());
+      throw UnexpectedApiException();
+    }
+  }
+
+  Future<VirtualZoneEntity> create(VirtualZoneEntity virtualZoneEntity) async {
     VirtualZoneDto virtualZoneDto =
         VirtualZoneDto.fromEntity(virtualZoneEntity);
     try {
       final response = await http.post(
         '/virtualZone',
-        body: virtualZoneDto.toJson(),
+        body: virtualZoneDto.toMap(),
       );
 
       validateResponse(
         response: response,
-        statusCodes: [200],
+        statusCodes: [201],
       );
 
       return VirtualZoneDto.fromMap(response.data);
@@ -31,17 +60,17 @@ class VirtualZoneProvider extends BaseProvider {
       rethrow;
     } catch (e) {
       logError(e.toString());
-      return null;
+      throw UnexpectedApiException();
     }
   }
 
-  Future<VirtualZoneEntity?> update(VirtualZoneEntity virtualZoneEntity) async {
+  Future<VirtualZoneEntity> update(VirtualZoneEntity virtualZoneEntity) async {
     VirtualZoneDto virtualZoneDto =
         VirtualZoneDto.fromEntity(virtualZoneEntity);
     try {
       final response = await http.put(
         '/virtualZone',
-        body: virtualZoneDto.toJson(),
+        body: virtualZoneDto.toMap(),
       );
 
       validateResponse(
@@ -56,11 +85,11 @@ class VirtualZoneProvider extends BaseProvider {
       rethrow;
     } catch (e) {
       logError(e.toString());
-      return null;
+      throw UnexpectedApiException();
     }
   }
 
-  Future<VirtualZoneEntity?> fetchById(int virtualZoneId) async {
+  Future<VirtualZoneEntity> fetchById(int virtualZoneId) async {
     try {
       final response = await http.get(
         '/virtualZone/$virtualZoneId',
@@ -78,7 +107,7 @@ class VirtualZoneProvider extends BaseProvider {
       rethrow;
     } catch (e) {
       logError(e.toString());
-      return null;
+      throw UnexpectedApiException();
     }
   }
 }
